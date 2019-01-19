2e2d7cf223d6521c8477fcba1b561eb0cd6ae382
Frank <asdj4030@gmail.com>
2019-01-19 22:34:59 +0800

    add 14 167

---
+++ src/com/zwf/TwoSumII.java
@@ -0,0 +1,22 @@
+package com.zwf;
+
+/**
+ * 167
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        for (int i = 0, j = numbers.length - 1; i < j;) {
+            if (numbers[i] + numbers[j] == target) {
+                result[0] = i+1;
+                result[1] = j+1;
+                return result;
+            } else if (numbers[i] + numbers[j] > target)
+                j--;
+            else if (numbers[i] + numbers[j] < target)
+                i++;
+        }
+        return result;
+    }
+
+}
