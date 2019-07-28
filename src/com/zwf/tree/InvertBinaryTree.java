f7886f59254b443d597e50dbb2624b67bac19d9f
Frank <asdj4030@gmail.com>
2019-07-28 22:08:11 +0800

    add 226

---
+++ src/com/zwf/tree/InvertBinaryTree.java
@@ -0,0 +1,19 @@
+package com.zwf.tree;
+
+/**
+ * 226 二叉树反转
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+
+}
